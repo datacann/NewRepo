cc361e827c216dafb65cc4c6b3e5cacfb5f401e4
atacan <atacan@LENOVO-PC>
2021-01-25 14:45:20 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,Q>
+    {
+        T[] takim ;
+        Q[] oyuncu;
+        public MyDictionary()
+        {
+            takim = new T[0];
+            oyuncu = new Q[0];
+
+           
+        }
+        T[] temptakim;
+        Q[] tempoyuncu;
+        public void Add (T takim1 , Q oyuncu1)
+        {
+            temptakim = takim;
+            tempoyuncu = oyuncu;
+
+            takim = new T[takim.Length + 1];
+            oyuncu = new Q[oyuncu.Length + 1];
+
+            for(int i = 0; i<temptakim.Length; i++)
+            {
+                takim[i] = temptakim[i];
+            }
+            for (int i = 0; i < temptakim.Length; i++)
+            {
+                oyuncu[i] = tempoyuncu[i];
+            }
+
+            takim[takim.Length - 1] = takim1;
+            oyuncu[oyuncu.Length - 1] = oyuncu1;
+        }
+        public void Ekran()
+        {
+            for (int i = 0; i < takim.Length; i++)
+            {
+                Console.WriteLine("takım: " + takim[i] + "---" + " oyuncu: " + oyuncu[i]);
+            }
+        }
+        public void Count()
+        {
+            Console.WriteLine(takim.Length);
+        }
+
+
+    }
+
+      
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> x = new MyDictionary<string, string>();
+            x.Add("nets", "harden");
+            x.Add("nets", "durant");
+            x.Add("dallas", "luka");
+            x.Ekran();
+            x.Count();
+        }
+    }
+}
